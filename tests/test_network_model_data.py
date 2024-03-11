8707c7e86e3caec7fd46cb32b5b026045b652016
Jeff H <jeff@westernspark.us>
2024-03-11 12:59:45 -0700

    Added Additional Test for Source Tracing from DB

--- tests/test_network_model_data.py
+++ tests/test_network_model_data.py
@@ -33,69 +33,128 @@ import pytest
 from temoa.temoa_model.model_checking import network_model_data
 from temoa.temoa_model.model_checking.source_check import CommodityNetwork
 
+# a couple of test cases with diagrams in the flow...
+params = [
+    # let's model this basic faulty network as a trial:
 
-# let's model this faulty network as a trial:
+    #     - t4(2) -> p3
+    #   /
+    # s1 -> t1 -> p1 -> t2 -> d1
+    #                        /
+    #             p2 -> t3  -
+    #
+    #             p2 -> t5 -> d2
 
-#     - t4(2) -> p3
-#   /
-# s1 -> t1 -> p1 -> t2 -> d1
-#                        /
-#             p2 -> t3  -
-#
-#             p2 -> t5 -> d2
-
-# the above should produce:
-# 2 valid techs, t1, t2
-# 2 supply-side orphans (both instances of t4 of differing vintage)
-# 1 demand-side orpha: t3
+    # the above should produce:
+    # 2 valid techs, t1, t2
+    # 2 supply-side orphans (both instances of t4 of differing vintage)
+    # 1 demand-side orphan: t3
+    {
+        'name': 'basic',
+        'data': [
+            [(t,) for t in ['s1', 'p1', 'p2', 'p3', 'd1', 'd2']],  # all commodities
+            [
+                (t,)
+                for t in [
+                    's1',
+                ]
+            ],  # sources
+            [('R1', 2020, 'd1'), ('R1', 2020, 'd2')],  # demands
+            [
+                ('R1', 's1', 't4', 2000, 'p3', 100),
+                ('R1', 's1', 't4', 1990, 'p3', 100),
+                ('R1', 's1', 't1', 2000, 'p1', 100),
+                ('R1', 'p1', 't2', 2000, 'd1', 100),
+                ('R1', 'p2', 't3', 2000, 'd1', 100),
+                ('R1', 'p2', 't5', 2000, 'd2', 100),
+            ],  # techs
+            [(2020,)],  # periods
+            [],  # no linked techs
+        ],
+        'res': {
+            'demands': 2,
+            'techs': 6,
+            'valid_techs': 2,
+            'demand_orphans': 2,
+            'other_orphans': 2,
+            'unsupported_demands': {'d2'},
+        },
+    },
+    #  p1 -> driven -> d2
+    #        |
+    #     - t4 -> p3
+    #   /
+    # s1 -> t1 -> d1
+    #
+    # bad link tech set.  t4 should be other orphan and the linked "driven" should be a demand side orphan
+    {
+        'name': 'bad linked tech',
+        'data': [
+            [(t,) for t in ['s1', 'p3', 'd1', 'd2']],  # all commodities
+            [
+                (t,)
+                for t in [
+                    's1',
+                ]
+            ],  # sources
+            [('R1', 2020, 'd1'), ('R1', 2020, 'd2')],  # demands
+            [
+                ('R1', 's1', 't4', 2000, 'p3', 100),
+                ('R1', 'p1', 'driven', 1990, 'd2', 100),
+                ('R1', 's1', 't1', 2000, 'd1', 100),
+            ],  # techs
+            [(2020,)],  # periods
+            [('R1', 't4', 'nox', 'driven')],  # t4 drives 'driven' with 'nox' emission
+        ],
+        'res': {
+            'demands': 2,
+            'techs': 3,
+            'valid_techs': 1,
+            'demand_orphans': 1,  # the linked tech, which will be orphaned after t4 is removed
+            'other_orphans': 1,  # t4
+            'unsupported_demands': {'d2'},
+        },
+    },
+]
 
 
 # we need a small fixture to simulate the database here
-@pytest.fixture
-def mock_db_connection():
-    data = [
-        [(t,) for t in ['s1', 'p1', 'p2', 'p3', 'd1']],  # all commodities
-        [
-            (t,)
-            for t in [
-                's1',
-            ]
-        ],  # sources
-        [('R1', 2020, 'd1'), ('R1', 2020, 'd2')],  # demands
-        [
-            ('R1', 's1', 't4', 2000, 'p3', 100),
-            ('R1', 's1', 't4', 1990, 'p3', 100),
-            ('R1', 's1', 't1', 2000, 'p1', 100),
-            ('R1', 'p1', 't2', 2000, 'd1', 100),
-            ('R1', 'p2', 't3', 2000, 'd1', 100),
-            ('R1', 'p2', 't5', 2000, 'd2', 100),
-        ],  # techs
-        [(2020,)],  # periods
-        [],  # no linked techs
-    ]
-
+@pytest.fixture()
+def mock_db_connection(request):
     mock_con = MagicMock()
     mock_cursor = MagicMock()
     mock_con.cursor.return_value = mock_cursor
     mock_execute = MagicMock()
     mock_cursor.execute.return_value = mock_execute
-    mock_execute.fetchall.side_effect = data
-    return mock_con
+    mock_execute.fetchall.side_effect = request.param['data']
+    return mock_con, request.param['res']
 
 
+@pytest.mark.parametrize(
+    'mock_db_connection', params, indirect=True, ids=[d['name'] for d in params]
+)
 def test__build_from_db(mock_db_connection):
-    network_data = network_model_data._build_from_db(mock_db_connection)
-    assert len(network_data.demand_commodities) == 1, 'only 1 demand'
-    assert len(network_data.available_techs['R1', 2020]) == 6, '6 techs are available'
+    """test a couple values in the load"""
+    conn, expected = mock_db_connection
+    network_data = network_model_data._build_from_db(conn)
+    assert len(network_data.demand_commodities) == expected['demands'], 'demand count failed'
+    assert (
+        len(network_data.available_techs['R1', 2020]) == expected['techs']
+    ), '6 techs are available'
 
 
+@pytest.mark.parametrize(
+    'mock_db_connection', params, indirect=True, ids=[d['name'] for d in params]
+)
 def test_source_trace(mock_db_connection):
-    network_data = network_model_data._build_from_db(mock_db_connection)
+    """analyze the network and check results"""
+    conn, expected = mock_db_connection
+    network_data = network_model_data._build_from_db(conn)
     cn = CommodityNetwork(region='R1', period=2020, model_data=network_data)
     cn.analyze_network()
 
     # test the outputs
-    assert len(cn.get_valid_tech()) == 2
-    assert len(cn.get_demand_side_orphans()) == 2
-    assert len(cn.get_other_orphans()) == 2
-    assert 'd2' in cn.unsupported_demands(), 'd2 has no techs driving it'
+    assert len(cn.get_valid_tech()) == expected['valid_techs'], 'should have this many valid techs'
+    assert len(cn.get_demand_side_orphans()) == expected['other_orphans'], 'demand orphans'
+    assert len(cn.get_other_orphans()) == expected['other_orphans'], 'other orphans'
+    assert cn.unsupported_demands() == expected['unsupported_demands'], 'unsupported demands'
